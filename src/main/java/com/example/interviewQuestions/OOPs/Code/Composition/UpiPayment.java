b3a8648b474cfa202859119fe43aff2b6fa3abb6
loser-joker17 <vkumarjnv1@gmail.com>
2026-06-30 22:51:41 +0530

    added the oops interface and abstract discussion along with situational questions

--- src/main/java/com/example/interviewQuestions/OOPs/Code/Composition/UpiPayment.java
+++ src/main/java/com/example/interviewQuestions/OOPs/Code/Composition/UpiPayment.java
@@ -3,6 +3,6 @@ package com.example.interviewQuestions.OOPs.Code.Composition;
 public class UpiPayment implements PaymentProcessor{
     @Override
     public void pay(){
-        System.out.println("Payment is done through UPI Transfer");
+        System.out.println("Payment is done through UPI Transfer ");
     }
 }
